a481ce8eb87b7bb8845ea2eccd174c6c4c9d1930
Brown, Chris, Vodafone Americas <chris.brown@vodafone.com>
2015-11-01 17:38:20 -0800

    no message

--- Pizza/FullMenuViewController.swift
+++ Pizza/FullMenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FullMenuViewController: UIViewController {
+class FullMenuViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
